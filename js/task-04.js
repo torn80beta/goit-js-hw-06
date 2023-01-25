df5a5b090b0dab0bdfe8e9c5817226426d022e78
Alex <103459735+torn80beta@users.noreply.github.com>
2023-01-25 23:54:06 +0200

    task 4 refactor

--- js/task-04.js
+++ js/task-04.js
@@ -9,10 +9,24 @@ incrementButton.addEventListener('click', onIncrementButtonClick);
 
 function onDecrementButtonClick() {
     counterValue -= 1;
+    counterColor()
     counterEl.textContent = counterValue;
 }
 
 function onIncrementButtonClick() {
     counterValue += 1;
+    counterColor()
     counterEl.textContent = counterValue;
+
+}
+
+// Additional practice :))
+function counterColor() {
+    if (counterValue === 0) {
+        counterEl.style.color = 'rgb(252, 252, 252)';
+    } else if (counterValue < 0) {
+        counterEl.style.color = 'rgb(255, 8, 8)';
+    } else {
+        counterEl.style.color = 'rgb(98, 255, 8)';
+    }
 }
