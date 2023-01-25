1821e42adfeb82005b123ce31891e87a985591b0
Alex <103459735+torn80beta@users.noreply.github.com>
2023-01-25 19:19:40 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('form.login-form');
 
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        window.alert("All fields must be filled!");
+    } else {
+        const submitData = { 'email': email.value, 'password': password.value };
+        console.log(submitData);
+        event.currentTarget.reset();
+    }
+}
