4ced348358e98461d4e64e3e678734663f837a3e
Alex <103459735+torn80beta@users.noreply.github.com>
2023-01-27 00:44:38 +0200

    final fixes

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,8 @@
 const categories = document.querySelectorAll('#categories .item');
 console.log(`Number of categories: ${categories.length}`);
-console.log('');
 
 
 for (const cat of categories) {
     console.log(`Category: ` + cat.querySelector('h2').textContent);
     console.log(`Elements: ` + cat.querySelectorAll('li').length);
-    console.log('');
 }
--- js/task-10.js
+++ js/task-10.js
@@ -2,6 +2,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+
 const numberOfElements = document.querySelector('input');
 const createButtonEl = document.querySelector('button[data-create]');
 const destroyButtonEl = document.querySelector('button[data-destroy]')
@@ -9,24 +10,25 @@ const boxesEl = document.querySelector('#boxes');
 let num = 0;
 let size = 20;
 
+
 numberOfElements.addEventListener('input', event => {
   num = event.currentTarget.value;
 });
-
 createButtonEl.addEventListener('click', onCreateButtonClick);
 destroyButtonEl.addEventListener('click', onDestroyButtonClick);
 
 
 function onCreateButtonClick() {
+  onDestroyButtonClick();
   const markup = Array(Number(num)).fill('').map(element =>
     `<div style="width:${size += 10}px;height:${size}px;background-color:${getRandomHexColor()}">${size}</div>`).join('');
   boxesEl.insertAdjacentHTML("afterbegin", markup);
+  num = 0;
+  size = 20;
+  numberOfElements.value = num;
 }
 
 
 function onDestroyButtonClick() {
   boxesEl.innerHTML = '';
-  num = 0;
-  size = 20;
-  numberOfElements.value = num;
 }
