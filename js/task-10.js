39f783ceb9a4a1c39c707298bcad21e4a2a8c2e3
Alex <103459735+torn80beta@users.noreply.github.com>
2023-01-26 19:44:32 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const numberOfElements = document.querySelector('input');
+const createButtonEl = document.querySelector('button[data-create]');
+const boxesEl = document.querySelector('#boxes');
+let num = 0;
+
+numberOfElements.addEventListener('input', event => {
+  num = event.currentTarget.value;
+  console.log(num);
+})
+
+createButtonEl.addEventListener('click', onCreateButtonClick);
+
+function onCreateButtonClick() {
+  let size = 20;
+  const collection = Array().fill('');
+  console.log(collection);
+  let boxes = collection.map(element => {
+    let color = getRandomHexColor();
+    size += 10;
+    return `<div width="${size}px" height="${size}px" style="color:${color}"></div>`;
+  });
+
+  boxesEl.insertAdjacentHTML("afterbegin", ...boxes);
+  console.log(boxes);
+}
+
+
+
+function createBoxes(amount) {
+  let size = 20;
+  const collection = Array(amount).fill('');
+  return collection.map(element => {
+    let color = getRandomHexColor();
+    size += 10;
+    return `<div width="${size}px" height="${size}px" style="color:${color}"></div>`;
+  });
+}
+
+
